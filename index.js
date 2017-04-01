fcaa80b35c798ffb962cb13a06054af8aa7c2997
Jonathan Terry <jterry@hugeinc.com>
2017-03-31 23:30:10 -0400

    created separate store for agents

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import hyperquext from 'hyperquext';
+import { agents } from './data/agentStore.json';
+// import phantomjs from 'phantomjs-prebuilt';
 
 const hyperquextDirect = hyperquext.decorators.hyperquextDirect;
 const r = hyperquextDirect(hyperquext);
-// const phantomjs = 'phantomjs-prebuilt';
 // const wdOpts = { desiredCapabilities: { browserName: 'phantomjs' } };
-const headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1588.0 Safari/537.36'
-}
 
-r('http://www.google.com', {headers: headers, maxRedirects: 10}, (err, res) => {
-	console.log(res);
+r('http://www.google.com', {headers: agents[1], maxRedirects: 10}, (err, res) => {
+	console.log(agents[1]);
+	console.log(res.statusCode, res.statusMessage);
+	console.log(res.request.uri, "\n");
 });
